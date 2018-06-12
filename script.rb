13727a872b7af9b102b550d343dccb61295e9d4c
Jane Sternbach <janester@github.com>
2018-06-11 20:22:05 -0400

    adding files 0

--- script.rb
+++ script.rb
@@ -3,7 +3,7 @@ require 'pry-byebug'
 
 g = Git.open('/Users/janester/Documents/code/dev-repos/test-public-repo/')
 
-4.times do |commit_count|
+6.times do |commit_count|
   puts "making directories..."
   `mkdir test_files`
   `mkdir test_files/lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_aliquam_ultricies_accumsan_velit_vitae_scelerisque_ex_egestas_in_proin_pellentesque_felis_ac_tellus_gravida_ut_viverra_purus_feugiat_pellentesque_habitant_morbi_tristique_senectus_et_netus_et_malesuad`
