e0ee491a5c57aea702b2cf4da400e18968e2432a
Jane Sternbach <janester@github.com>
2018-06-12 12:30:03 -0400

    adding files 1

--- script.rb
+++ script.rb
@@ -10,14 +10,14 @@ g = Git.open('/Users/janester/Documents/code/dev-repos/test-public-repo/')
 
   puts "making files..."
   3000.times do |file_count|
-    `touch test_files/lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_aliquam_ultricies_accumsan_velit_vitae_scelerisque_ex_egestas_in_proin_pellentesque_felis_ac_tellus_gravida_ut_viverra_purus_feugiat_pellentesque_habitant_morbi_tristique_senectus_#{file_count}.txt`
+    `touch test_files/lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_aliquam_ultricies_accumsan_velit_vitae_scelerisque_ex_egestas_in_proin_pellentesque_felis_ac_tellus_gravida_ut_viverra_purus_feugiat_pellentesque_habitant_morbi_tristique_senectus_#{file_count + 1}.txt`
   end
 
   puts "adding files to git..."
   `git add .`
 
   puts "committing files to git..."
-  g.commit("adding files #{commit_count}")
+  g.commit("adding files #{commit_count + 1}")
 
   puts "removing all of the files..."
   `rm -rf test_files/`
@@ -26,5 +26,5 @@ g = Git.open('/Users/janester/Documents/code/dev-repos/test-public-repo/')
   `git add .`
 
   puts "committing files to git..."
-  g.commit("removing files #{commit_count}")
+  g.commit("removing files #{commit_count + 1}")
 end
